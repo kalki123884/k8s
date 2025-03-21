687d58227f60b7cb6b285ab7230ba83013261e87
kalki123884 <sivasankar.inkollu@yahoo.com>
2025-03-21 17:32:12 -0400

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI()
+@app.get("/")
+def test():
+  message="this is my message"
+  return message
+@app.get("/user/{username}")
+def test2(username: str):
+  message="This is Canary deployment"
+  return message
+if __name__ == "__main__":
+   uvicorn.run("test1:app",host="0.0.0.0",port=8000)
