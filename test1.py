cdbe8999356c12025edb0ae96a57e15faf3d7426
kalki123884 <sivasankar.inkollu@yahoo.com>
2025-03-21 17:23:48 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,5 +5,9 @@ app = FastAPI()
 def test():
   message="this is my message"
   return message
+@app.get("/user/{username}")
+def test2(username: str):
+  message="This is Regular deployment"
+  return message
 if __name__ == "__main__":
    uvicorn.run("test1:app",host="0.0.0.0",port=8000)
